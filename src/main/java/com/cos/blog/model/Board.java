5e8b4a20649db59360122ebd3280f5b99728eecb
know7701 <know7701@gmail.com>
2022-12-16 09:59:51 +0900

    update

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,53 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Board {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob
+	private String content;
+	
+	@ColumnDefault("0")
+	private int count;
+	
+	@ManyToOne // Many = Board, User = One
+	@JoinColumn(name="userId")
+	private User user;
+	
+	@OneToMany(mappedBy = "board") // mappedBy 는 fk 키가 아니라고 명시해두는것.
+	private List<Reply> reply;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
